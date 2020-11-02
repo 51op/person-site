ccc1da98507e0e8c1c5a687150e14936e94bbaa8
test <test@u51.com>
2020-11-02 15:50:48 +0800

    first commit

--- src/components/articles/art01.vue
+++ src/components/articles/art01.vue
@@ -1,5 +1,9 @@
 <template>
+<div class="art-1">
 
+    <h2>汇总当下20k以上运维面试总结</h2>
+
+</div>
 </template>
 
 <script>
---
+++ src/views/details/art01.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="art-1">
+
+        <h2>{{this.$route.params}}</h2>
+
+
+</div>
+</template>
+
+<script>
+    export  default {
+        name:'art01',
+        data(){
+           return{
+               // map: {
+               //     123: {
+               //         title: '123'
+               //     },
+               //     1234: {
+               //          title: 1234
+               //     }
+               // },
+           }
+
+        },
+    }
+</script>
+<style scoped>
+
+
+</style>
